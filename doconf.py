6f4421d305f6110eca703fcecf8eb0a2b5c6a10c
David Breese <dabreese00@gmail.com>
2026-01-10 03:39:11 -0600

    Add config file

--- doconf.py
+++ doconf.py
@@ -1,9 +1,30 @@
 import os
+import re
 from conf import Configs
 
+RE_CONFIGLINE = r"^([^=]+)=(.+)$"
+CONFIG_FILENAME = ".wallabaggins.conf"
+ALLOWED_KEYS = [
+    "serverurl",
+    "username",
+    "password",
+    "client",
+    "secret"
+]
+
+def handle_invalid_config():
+    print("Invalid config file.")
+    exit(1)
+
+def load_conf(filename):
+    r = re.compile(RE_CONFIGLINE)
+    with open(filename, 'r') as f:
+        for line in f:
+            m = r.match(line)
+            key, value = m.groups()
+            if not key in ALLOWED_KEYS:
+                handle_invalid_config()
+            setattr(Configs, key, value)
+
 def do_conf():
-    Configs.username = os.environ.get("un")
-    Configs.password = os.environ.get("PW")
-    Configs.serverurl = "https://app.wallabag.it"
-    Configs.client = os.environ.get("client")
-    Configs.secret = os.environ.get("SECRET")
+    load_conf(CONFIG_FILENAME)
